339727f2b1594a918dcb6800cf64454a7834cc36
zara-sh <zara.regist@gmail.com>
2018-03-22 15:20:51 +0900

    add show action and flsh msg

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,11 +7,15 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.new(todo_params)
     if @todo.save
-    # @todo.save
+      flash[:notice] = "Todo was created successfully!"
       redirect_to todo_path(@todo)
     else
       render "new"
     end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
   end
 
   private
