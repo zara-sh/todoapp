65223a644a6f3873396d6b80b4223f5dab320b9d
zara-sh <zara.regist@gmail.com>
2018-05-04 18:28:39 +0900

    add association and foreign key

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+
+  belogs_to :User
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317152849) do
+ActiveRecord::Schema.define(version: 20180504092115) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
